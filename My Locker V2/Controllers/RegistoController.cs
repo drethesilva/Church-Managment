d3dbdd5b1753dd2d99fe64a3ad29c15f7e12bc20
asilva-i9 <asilva@innovagency.com>
2020-10-13 18:12:55 +0100

    First part on buiding the registry

---
+++ My Locker V2/Controllers/RegistoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace My_Locker_V2.Controllers
+{
+    public class RegistoController : Controller
+    {
+        // GET: Registo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submited(string Nome , string Apelido , string Email , string Password , bool Membro)
+        {
+          
+
+            return View();
+        }
+    }
+}
