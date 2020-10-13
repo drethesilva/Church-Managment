6f2e4a75148d37c4f18e70af9e8d0e1b715d6dce
asilva-i9 <asilva@innovagency.com>
2020-10-13 11:57:26 +0100

    Inicio e Conexão à BD

--- My Locker V2/Controllers/HomeController.cs
+++ My Locker V2/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using My_Locker_V2.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,8 @@ namespace My_Locker_V2.Controllers
 {
     public class HomeController : Controller
     {
+        MasterDB context = new MasterDB();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,19 @@ namespace My_Locker_V2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult List()
+        {
+            // se for necessario passar parametros 
+            //SqlParameter[] param = new SqlParameter[]
+            //{
+            //    new SqlParameter("",0),
+            //    new SqlParameter("",0)
+            //};
+
+            var data = context.Database.SqlQuery<Users>("select_users_and_info").ToList();
+            return View(data);
         }
     }
 }
