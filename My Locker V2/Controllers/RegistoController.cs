55261c11f7f2c33fdc17b38d9b6986c4aff8bf34
asilva-i9 <asilva@innovagency.com>
2020-10-14 19:50:49 +0100

    Email p2

--- My Locker V2/Controllers/RegistoController.cs
+++ My Locker V2/Controllers/RegistoController.cs
@@ -20,49 +20,70 @@ namespace My_Locker_V2.Controllers
             return View();
         }
 
-        public ActionResult Submited(string Nome , string Apelido , string Email , string Password , bool Membro)
+        [HttpPost]
+        public ActionResult Submited(Models.Utilizadores formData)
         {
-            try{
-                if(Email != "")
+            try
+            {
+                
+
+                if (formData.Email != "")
                 {
                     var data = context.Database.SqlQuery<Utilizadores>("showUSers").ToList();
-                    
-                    foreach(var i in data)
+
+                    foreach (var i in data)
                     {
-                        if (i.Email == (Email))
+                        var serverEmail = i.Email.ToLower();
+                        var userEmail = formData.Email.ToLower();
+
+                        if (serverEmail == (userEmail))
                         {
                             hasEmail = true;
                         }
                     }
                     hasEmail = false;
 
+                    if(hasEmail == false)
+                    {
+
+                    }
+                    else
+                    {
+                        // Mandar msg de erro 
+                    }
 
                 }
 
             }
             catch (Exception er)
             {
-            
-            }
 
-            /*
-            SqlParameter[] param = new SqlParameter[]
-            {
-                new SqlParameter("@Nome",Nome),
-                new SqlParameter("@Apelido",Apelido),
-                new SqlParameter("@Email",Email),
-                new SqlParameter("@Password",Password),
-                new SqlParameter("@Membro","0"),
-            };
-            // var data = context.Database.SqlQuery<Users>("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro",param);
-            try
-            {
-                context.Database.ExecuteSqlCommand("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro", param);
             }
-            catch (Exception er) { }
-            */
 
             return View();
+
+
         }
+
+
+        /*
+        SqlParameter[] param = new SqlParameter[]
+        {
+            new SqlParameter("@Nome",Nome),
+            new SqlParameter("@Apelido",Apelido),
+            new SqlParameter("@Email",Email),
+            new SqlParameter("@Password",Password),
+            new SqlParameter("@Membro","0"),
+        };
+        // var data = context.Database.SqlQuery<Users>("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro",param);
+        try
+        {
+            context.Database.ExecuteSqlCommand("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro", param);
+        }
+        catch (Exception er) { }
+        */
+
+
     }
 }
+    
--- My Locker V2/Models/Utilizadores.cs
+++ My Locker V2/Models/Utilizadores.cs
@@ -11,14 +11,23 @@ namespace My_Locker_V2.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Utilizadores
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o seu Nome!" , AllowEmptyStrings = false)]
         public string Nome { get; set; }
         public string Apelido { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o seu email!" , AllowEmptyStrings = false)]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Por favor digite uma password segura!", AllowEmptyStrings = false)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Por favor indique se é membro ou não!", AllowEmptyStrings = false)]
         public Nullable<bool> Membro { get; set; }
     }
 }
