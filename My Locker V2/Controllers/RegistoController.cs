7bce6401649bb8f1f7226ddb567d6b9058c399db
asilva-i9 <asilva@innovagency.com>
2020-10-14 12:36:32 +0100

    Email Check p1

--- My Locker V2/Controllers/RegistoController.cs
+++ My Locker V2/Controllers/RegistoController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using My_Locker_V2.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +11,10 @@ namespace My_Locker_V2.Controllers
     public class RegistoController : Controller
     {
         // GET: Registo
+
+        MasterDB context = new MasterDB();
+        bool hasEmail;
+
         public ActionResult Index()
         {
             return View();
@@ -16,7 +22,45 @@ namespace My_Locker_V2.Controllers
 
         public ActionResult Submited(string Nome , string Apelido , string Email , string Password , bool Membro)
         {
-          
+            try{
+                if(Email != "")
+                {
+                    var data = context.Database.SqlQuery<Utilizadores>("showUSers").ToList();
+                    
+                    foreach(var i in data)
+                    {
+                        if (i.Email == (Email))
+                        {
+                            hasEmail = true;
+                        }
+                    }
+                    hasEmail = false;
+
+
+                }
+
+            }
+            catch (Exception er)
+            {
+            
+            }
+
+            /*
+            SqlParameter[] param = new SqlParameter[]
+            {
+                new SqlParameter("@Nome",Nome),
+                new SqlParameter("@Apelido",Apelido),
+                new SqlParameter("@Email",Email),
+                new SqlParameter("@Password",Password),
+                new SqlParameter("@Membro","0"),
+            };
+            // var data = context.Database.SqlQuery<Users>("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro",param);
+            try
+            {
+                context.Database.ExecuteSqlCommand("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro", param);
+            }
+            catch (Exception er) { }
+            */
 
             return View();
         }
