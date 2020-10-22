335a26f5897af19d9aeef873586fedb4bd73800e
asilva-i9 <asilva@innovagency.com>
2020-10-22 13:28:10 +0100

    Registo quase completo so falta dar redirect pra outro sitio

--- My Locker V2/Controllers/RegistoController.cs
+++ My Locker V2/Controllers/RegistoController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,38 +22,83 @@ namespace My_Locker_V2.Controllers
             return View();
         }
 
+        public static string Encrypt(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding encoder = new UTF8Encoding();
+            Byte[] originalBytes = encoder.GetBytes(password);
+            Byte[] encodedBytes = md5.ComputeHash(originalBytes);
+            password = BitConverter.ToString(encodedBytes).Replace("-", "");
+            var result = password.ToLower();
+
+            return result;
+        }
+
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Submited(Models.Utilizadores formData)
         {
             try
             {
-                
-
-                if (formData.Email != "")
+                if (formData.Email.Contains("@"))
                 {
-                    var data = context.Database.SqlQuery<Utilizadores>("showUSers").ToList();
-
-                    foreach (var i in data)
+                    try
                     {
-                        var serverEmail = i.Email.ToLower();
-                        var userEmail = formData.Email.ToLower();
+                        var data = context.Database.SqlQuery<Utilizadores>("showUSers").ToList();
 
-                        if (serverEmail == (userEmail))
+                        foreach (var i in data)
                         {
-                            hasEmail = true;
+                            var serverEmail = i.Email.ToLower();
+                            var userEmail = formData.Email.ToLower();
+
+                            if (serverEmail == (userEmail))
+                            {
+                                hasEmail = true;
+                            }
+                            else { hasEmail = false; }
                         }
                     }
-                    hasEmail = false;
+                    catch (Exception er) { }
+                           
+
 
                     if(hasEmail == false)
                     {
+                        // Encriptar password
+                        Encrypt(formData.Password);
+
+                        int membro;
+
+                        if (formData.Membro == true) { membro = 1; } else { membro = 0; }
+
+                        SqlParameter[] param = new SqlParameter[]{new SqlParameter("@Nome", formData.Nome),
+                            new SqlParameter("@Apelido", formData.Apelido),
+                            new SqlParameter("@Email", formData.Email),
+                            new SqlParameter("@Password", formData.Password),
+                            new SqlParameter("@Membro", membro), };
+{
+                            
+};
+                        var data = context.Database.SqlQuery<Users>("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro",param);
+                        try
+                        {
+                            context.Database.ExecuteSqlCommand("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro", param);
+                        }
+                        catch (Exception er) { }
 
                     }
                     else
                     {
-                        // Mandar msg de erro 
+                        ModelState.AddModelError("Email", "Email já registado, Deseja fazer login?");
+                        return View("Index", formData);
                     }
 
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "Email Inexistente");
+                    return View("Index",formData);
                 }
 
             }
@@ -66,22 +113,9 @@ namespace My_Locker_V2.Controllers
         }
 
 
-        /*
-        SqlParameter[] param = new SqlParameter[]
-        {
-            new SqlParameter("@Nome",Nome),
-            new SqlParameter("@Apelido",Apelido),
-            new SqlParameter("@Email",Email),
-            new SqlParameter("@Password",Password),
-            new SqlParameter("@Membro","0"),
-        };
-        // var data = context.Database.SqlQuery<Users>("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro",param);
-        try
-        {
-            context.Database.ExecuteSqlCommand("Add_Utilizador @Nome,@Apelido,@Email,@Password,@Membro", param);
-        }
-        catch (Exception er) { }
-        */
+        
+
+        
 
 
     }
